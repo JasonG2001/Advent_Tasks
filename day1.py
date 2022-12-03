8219e2630d48bde19f7d4ddbcdbb874f80504a61
jason <xiaohemc2001@gmail.com>
2022-12-03 20:20:51 +0000

    Refactor

--- day1.py
+++ day1.py
@@ -1,23 +1,23 @@
 from typing import List
 
 with open("elf_calories.txt") as f:
-    lines: List[str] = f.readlines()
+    calories: List[str] = [line.strip() for line in f.readlines()]
 
 elf: List[int] = []
 all_elves: List[List[int]] = []
 calorie: str
-for calorie in lines:
-    if calorie != "\n":
-        elf.append(int(calorie[:-1]))
+for calorie in calories:
+    if calorie != "":
+        elf.append(int(calorie))
     else:
         all_elves.append(elf)
         elf: List[int] = []
 
 list_of_sums: List[int] = [sum(x) for x in all_elves]
 
-print(max(list_of_sums))
+print(max(list_of_sums)) # 68442
 
-print(sum(sorted(list_of_sums, reverse=True)[:3]))
+print(sum(sorted(list_of_sums, reverse=True)[:3])) # 204837
 
 
 
