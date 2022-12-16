41399e0c03e20650f8c225acfd3bbebf0d4c554b
jason <xiaohemc2001@gmail.com>
2022-12-16 13:05:00 +0000

    Near finish#

--- day11.py
+++ day11.py
@@ -16,7 +16,7 @@ def make_monkey_rules(file: List[str]) -> Dict[str, Dict[str, str]]:
     rules: List[Dict[str, str]] = []
 
     line: str
-    for line in file:
+    for index, line in enumerate(file):
 
         if len(line) == 0:
             continue
@@ -32,6 +32,9 @@ def make_monkey_rules(file: List[str]) -> Dict[str, Dict[str, str]]:
                 rule: Dict[str, str] = {}
 
             rule[description[0]] = description[1]
+
+            if file[index] == file[-1]:
+                rules.append(rule) 
     return dict(zip(monkeys, rules))
 
 
@@ -41,19 +44,16 @@ def convert_to_queue(file: List[str]) -> Dict[str, Dict[str, Union[queue, str]]]
 
     rules: Dict[str]
     for rules in monkey_rules.values():
-        list_of_worry_level: List[int] = [int(number) for number in rules["Starting items"].split(",")]
+        
+        items_list: List[int] = [int(number) for number in rules["Starting items"].split(",")]
+        item_queue: queue = queue(items_list)
 
-        rules["Starting items"] = queue(list_of_worry_level)
+        rules["Starting items list"]: queue = item_queue
+
     return monkey_rules
 
 
-def make_inspections_dict(monkey_rules: Dict[str, Dict[str, Union[queue, str]]]) -> Dict[str, int]:
-
-    inspections: Dict[str, int] = {monkey:0 for monkey in monkey_rules}
-    return inspections
-
-
-def get_outcome(monkey_rules: Dict[str, Dict[str, Union[queue, str]]],
+def get_inspections(monkey_rules: Dict[str, Dict[str, Union[queue, str]]],
     inspections: Dict[str, int], number_of_rounds: int, current_rounds: int = 0) -> Dict[str, int]:
 
     if current_rounds == number_of_rounds:
@@ -63,9 +63,9 @@ def get_outcome(monkey_rules: Dict[str, Dict[str, Union[queue, str]]],
     rules: Dict[str, Union[queue, str]]
     for monkey, rules in monkey_rules.items():
 
-        starting_items: queue = rules["Starting items"]
+        starting_items: queue = rules["Starting items list"]
         operation: str = rules["Operation"][11]
-        factor: int = int(rules["Operation"][13:])
+        factor: str = rules["Operation"][13:]
         dividing_term: int = int(rules["Test"][14:])
         monkey_receiving_when_true: str = rules["If true"][-1]
         monkey_receiving_when_false: str = rules["If false"][-1]
@@ -75,31 +75,39 @@ def get_outcome(monkey_rules: Dict[str, Dict[str, Union[queue, str]]],
             
             if factor == "old":
                 factor: int = inspected_item
+
             if operation == "+":
-                inspected_item += factor
+                inspected_item += int(factor)
             else:
-                inspected_item *= factor
+                inspected_item *= int(factor)
 
             inspected_item //= 3
 
             if inspected_item % dividing_term == 0:
-                monkey_rules[monkey_receiving_when_true]["Starting items"].append(inspected_item)
+                monkey_rules[monkey_receiving_when_true]["Starting items list"].append(inspected_item)
             else:
-                monkey_rules[monkey_receiving_when_false]["Starting items"].append(inspected_item)
+                monkey_rules[monkey_receiving_when_false]["Starting items list"].append(inspected_item)
 
-            inspections[monkey] + 1
+            inspections[monkey] += 1
+
+    current_rounds += 1
+
+    return get_inspections(monkey_rules, inspections, number_of_rounds, current_rounds)
 
 
-        current_rounds += 1
+def get_monkey_business(inspections: Dict[str, int]) -> int:
+    ordered_inspections: List[int] = sorted(inspections.values(), reverse=True)
 
-        return get_outcome(monkey_rules, inspections, number_of_rounds, current_rounds)
+    return ordered_inspections[0] * ordered_inspections[1]
 
 
 if __name__ == "__main__":
     file: List[str] = open_file("day11.txt")
-    monkey_rules: Dict[str, Dict[str, Union[queue, str]]] = convert_to_queue(file)
-    #inspection_dict: Dict[str, int] = make_inspections_dict(monkey_rules)
-    inspection_dict: Dict[str, int] = defaultdict(int)
-    print(get_outcome(monkey_rules, inspection_dict, 20))
+    monkey_rules: Dict[str, Dict[str, Union[queue, str]]] = convert_to_queue(file)   
+    inspection_dict_template: Dict[str, int] = defaultdict(int)
+    inspections = get_inspections(monkey_rules, inspection_dict_template, 20)
+    print(inspections)
+    monkey_business = get_monkey_business(inspections)
+    print(monkey_business)
 
     
